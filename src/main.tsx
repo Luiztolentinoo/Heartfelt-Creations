ac5d7faf0eb4918f04d7349eaee346b20617af0f
Luiztolentinoo <luizinhoalves0202@gmail.com>
2026-09-06 14:14:19 -0400

    style: load report workflow styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Root from './Root'
 import './styles.css'
 import './public.css'
+import './request-workflow.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
